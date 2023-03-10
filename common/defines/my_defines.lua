75f17f4338427cb120fa5c844cf1715ca37cf2db
SovetovCumminist <89398225+SovetovCumminist@users.noreply.github.com>
2023-03-10 13:24:35 +0300

    first

---
+++ common/defines/my_defines.lua
@@ -0,0 +1,28 @@
+NDefines.NGame.START_DATE = "1950.1.1.1"
+NDefines.NGame.END_DATE = "2000.1.1.1"
+
+NDefines.NTechnology.BASE_TECH_COST = 80
+
+NDefines.NCountry.BASE_RESEARCH_SLOTS = 1
+NDefines.NCountry.NUCLEAR_PRODUCTION_SCALE = 365
+NDefines.NCountry.WEEKLY_STABILITY_GAIN = -0.01
+NDefines.NCountry.WEEKLY_WAR_SUPPORT_GAIN = -0.01
+NDefines.NCountry.MAJOR_MIN_FACTORIES = 40
+
+NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 2.2
+
+NDefines.NDiplomacy.GUARANTEE_COST = 100
+
+NDefines.NMilitary.UNIT_UPKEEP_ATTRITION = 0.01
+NDefines.NMilitary.NUKE_MIN_DAMAGE_PERCENT = 0.75			
+NDefines.NMilitary.NUKE_MAX_DAMAGE_PERCENT = 1.25
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1500
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1500			
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1500
+
+NDefines.NGraphics.DRAW_FOW_CUTOFF = 0
+NDefines.NGraphics.DRAW_FOW_FADE_LENGTH = 0
+
+NDefines.NMilitary.PLANNING_DECAY = 0.01
+NDefines.NMilitary.PLANNING_GAIN = 0.02
+NDefines.NMilitary.PLANNING_MAX = 0.5 
